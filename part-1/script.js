2aef5f1fc2a4a805c6cb809a3df1ce9aefcf537a
PariBainsla <paribainsla161@gmail.com>
2025-07-20 13:11:20 -0700

    selesction sort

--- part-1/script.js
+++ part-1/script.js
@@ -57,15 +57,33 @@
 //----------------- SORTING -----------------
 
 //-----Bubble Sort
-let arr = [10, 5, 1, 12, 3]
+// let arr = [10, 5, 1, 12, 3]
+// let n = arr.length;
+// for (let i = 0; i <= n - 1; i++) {
+//     for (let j = 0; j < n - 1 - i; j++) {
+//         if (arr[j] > arr[j + 1]) {
+//             let temp = arr[j];
+//             arr[j] = arr[j + 1];
+//             arr[j + 1] = temp;
+//         }
+//     }
+// }
+// console.log(arr);
+
+//-----Selection Sort
+let arr = [10, 5,1,12,3];
 let n = arr.length;
-for (let i = 0; i <= n - 1; i++) {
-    for (let j = 0; j < n - 1 - i; j++) {
-        if (arr[j] > arr[j + 1]) {
-            let temp = arr[j];
-            arr[j] = arr[j + 1];
-            arr[j + 1] = temp;
+for(let i=0; i<n-1; i++){
+    let min = i;
+    for(let j=i+1; j<n; j++){
+        if(arr[min] > arr[j]){
+            min = j;
         }
+    }
+    if(min != i){
+        let temp = arr[min];
+        arr[min] = arr[i];
+        arr[i] = temp;
     }
 }
 console.log(arr);
