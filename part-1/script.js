f94461a6ad50818a3b8506c6f12a4599e2149e4d
PariBainsla <paribainsla161@gmail.com>
2025-07-21 10:25:07 -0700

    insertion sort

--- part-1/script.js
+++ part-1/script.js
@@ -71,19 +71,33 @@
 // console.log(arr);
 
 //-----Selection Sort
-let arr = [10, 5,1,12,3];
+// let arr = [10, 5,1,12,3];
+// let n = arr.length;
+// for(let i=0; i<n-1; i++){
+//     let min = i;
+//     for(let j=i+1; j<n; j++){
+//         if(arr[min] > arr[j]){
+//             min = j;
+//         }
+//     }
+//     if(min != i){
+//         let temp = arr[min];
+//         arr[min] = arr[i];
+//         arr[i] = temp;
+//     }
+// }
+// console.log(arr);
+
+//-----Insertion Sort
+let arr = [10,5,1,12,3]
 let n = arr.length;
-for(let i=0; i<n-1; i++){
-    let min = i;
-    for(let j=i+1; j<n; j++){
-        if(arr[min] > arr[j]){
-            min = j;
-        }
+for(let i=1; i<n; i++){
+    let key = arr[i];
+    let j = i-1;
+    while(arr[j]>key && j>=0){
+        arr[j+1] = arr[j];
+        j--;
     }
-    if(min != i){
-        let temp = arr[min];
-        arr[min] = arr[i];
-        arr[i] = temp;
-    }
+    arr[j+1] = key;
 }
 console.log(arr);
