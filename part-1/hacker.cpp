23e5b7a5d642e0a773cf90eb4b6efbbc46425b40
PariBainsla <paribainsla161@gmail.com>
2025-09-16 17:00:51 -0700

    c++ problem2

--- part-1/hacker.cpp
+++ part-1/hacker.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
-#include <iomanip>
-#include <limits>
+// #include <iomanip>
+// #include <limits>
 
-using namespace std;
+// using namespace std;
 
-int main() {
-    int i = 4;
-    double d = 4.0;
-    string s = "HackerRank ";
+// int main() {
+//     int i = 4;
+//     double d = 4.0;
+//     string s = "HackerRank ";
     
-    int i2;
-    double d2;
-    string s2;
+//     int i2;
+//     double d2;
+//     string s2;
     
-    cin>>i2;
-    cin>>d2;
-    cin.ignore();
-    getline(cin,s2);
+//     cin>>i2;
+//     cin>>d2;
+//     cin.ignore();
+//     getline(cin,s2);
 
-cout<<i+i2<<endl;
-cout<<fixed<<setprecision(1)<<d+d2<<endl;
-cout<<s+s2<<endl;
+// cout<<i+i2<<endl;
+// cout<<fixed<<setprecision(1)<<d+d2<<endl;
+// cout<<s+s2<<endl;
 
     
     
@@ -36,4 +36,19 @@ cout<<s+s2<<endl;
     // Concatenate and print the String variables on a new line
     // The 's' variable above should be printed first.
 
-    return 0;}
+    // return 0;}
+
+
+    //QUESTION
+
+    void solve(double meal_cost, int tip_percent, int tax_percent) {
+    // Calculate tip and tax
+    double tip = meal_cost * tip_percent / 100.0;
+    double tax = meal_cost * tax_percent / 100.0;
+
+    // Total cost
+    double total_cost = meal_cost + tip + tax;
+
+    // Print rounded total cost
+    cout << round(total_cost) << endl;
+}
