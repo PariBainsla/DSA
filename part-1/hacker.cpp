6edfc442631b88f76e8826959fa98c64df49d006
PariBainsla <paribainsla161@gmail.com>
2025-09-17 17:34:07 -0700

    Conditionals

--- part-1/hacker.cpp
+++ part-1/hacker.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+// #include <iostream>
 // #include <iomanip>
 // #include <limits>
 
@@ -8,11 +8,11 @@
 //     int i = 4;
 //     double d = 4.0;
 //     string s = "HackerRank ";
-    
+
 //     int i2;
 //     double d2;
 //     string s2;
-    
+
 //     cin>>i2;
 //     cin>>d2;
 //     cin.ignore();
@@ -22,33 +22,53 @@
 // cout<<fixed<<setprecision(1)<<d+d2<<endl;
 // cout<<s+s2<<endl;
 
-    
-    
-    // Declare second integer, double, and String variables.
-    
-    // Read and save an integer, double, and String to your variables.
-    // Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
-    
-    // Print the sum of both integer variables on a new line.
-    
-    // Print the sum of the double variables on a new line.
-    
-    // Concatenate and print the String variables on a new line
-    // The 's' variable above should be printed first.
+// Declare second integer, double, and String variables.
 
-    // return 0;}
+// Read and save an integer, double, and String to your variables.
+// Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
 
+// Print the sum of both integer variables on a new line.
 
-    //QUESTION
+// Print the sum of the double variables on a new line.
 
-    void solve(double meal_cost, int tip_percent, int tax_percent) {
-    // Calculate tip and tax
-    double tip = meal_cost * tip_percent / 100.0;
-    double tax = meal_cost * tax_percent / 100.0;
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
 
-    // Total cost
-    double total_cost = meal_cost + tip + tax;
+// return 0;}
 
-    // Print rounded total cost
-    cout << round(total_cost) << endl;
+// QUESTION
+
+//     void solve(double meal_cost, int tip_percent, int tax_percent) {
+//     double tip = meal_cost * tip_percent / 100.0;
+//     double tax = meal_cost * tax_percent / 100.0;
+
+//     double total_cost = meal_cost + tip + tax;
+
+//     cout << round(total_cost) << endl;
+// }
+
+// QUESTION
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    if (n % 2 != 0)
+    {
+        cout << "Weird";
+    }
+    else if (n >= 2 && n < 5)
+    {
+        cout << "not weird";
+    }
+    else if (n >= 6 && n <= 20)
+    {
+        cout << "weird";
+    }
+    else if (n > 20)
+    {
+        cout << "not weird";
+    }
+    return 0;
 }
